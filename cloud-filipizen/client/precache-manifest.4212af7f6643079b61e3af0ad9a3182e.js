2fc0889837cf241bc4cf8642a68c35c361e7a7bb
Jessie Zamora <jzamss@gmail.com>
2021-08-23 15:35:06 +0800

    cloud-filipizen-client: obo updates

---
+++ cloud-filipizen/client/precache-manifest.4212af7f6643079b61e3af0ad9a3182e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5535bd4146e877cfa80a3066113a4ba2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f2fdbf7e41995ab5516",
+    "url": "/static/css/2.a84fd3a7.chunk.css"
+  },
+  {
+    "revision": "96793b49007437ae8afa",
+    "url": "/static/css/main.f856e34a.chunk.css"
+  },
+  {
+    "revision": "5f2fdbf7e41995ab5516",
+    "url": "/static/js/2.63f3e69b.chunk.js"
+  },
+  {
+    "revision": "35404924d101c4742361a5c4a5f250c5",
+    "url": "/static/js/2.63f3e69b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f69986e118e8225956c",
+    "url": "/static/js/3.afc78525.chunk.js"
+  },
+  {
+    "revision": "75500a672dadcf0d3eda",
+    "url": "/static/js/4.c573e410.chunk.js"
+  },
+  {
+    "revision": "a0464bbb10a5c35d5e33",
+    "url": "/static/js/5.a52c3ecd.chunk.js"
+  },
+  {
+    "revision": "df63b0a161ab54be5284",
+    "url": "/static/js/6.e2879cfb.chunk.js"
+  },
+  {
+    "revision": "4589bf1c279347944e76",
+    "url": "/static/js/7.6527b136.chunk.js"
+  },
+  {
+    "revision": "96793b49007437ae8afa",
+    "url": "/static/js/main.3405372b.chunk.js"
+  },
+  {
+    "revision": "c20304e3bc29180c40c0",
+    "url": "/static/js/runtime-main.9d6d22e6.js"
+  },
+  {
+    "revision": "5fd85b9e136074ed166f46c783115828",
+    "url": "/static/media/filipizen.5fd85b9e.svg"
+  },
+  {
+    "revision": "3a1a1c3516668b3b794547646d9d297e",
+    "url": "/static/media/logo~FfTuzhZc.3a1a1c35.png"
+  }
+]);
