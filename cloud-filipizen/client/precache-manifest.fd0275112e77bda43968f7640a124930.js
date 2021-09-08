8ff2f283ddb70b3524f5d36fefe261693629a630
Jessie S. Zamora <jzamss@gmail.com>
2021-09-08 18:52:07 +0800

    filipizen-client: obo: set null values to 0 filipizen-client: rptis: fix advance payment support

---
+++ cloud-filipizen/client/precache-manifest.fd0275112e77bda43968f7640a124930.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1907cb192ce9bc32c004d18df283f4a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20023662576f3615e17f",
+    "url": "/static/css/2.a84fd3a7.chunk.css"
+  },
+  {
+    "revision": "3d4fb4189c8a322a3001",
+    "url": "/static/css/main.8d4fc355.chunk.css"
+  },
+  {
+    "revision": "20023662576f3615e17f",
+    "url": "/static/js/2.875beefb.chunk.js"
+  },
+  {
+    "revision": "9568e9db482b633f71981d83114e5099",
+    "url": "/static/js/2.875beefb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880523bd3957b5d8cb5c",
+    "url": "/static/js/3.318f3cc1.chunk.js"
+  },
+  {
+    "revision": "752de3de8b1f8bfae579",
+    "url": "/static/js/4.f3aee9f4.chunk.js"
+  },
+  {
+    "revision": "c02b9564570a8253d6db",
+    "url": "/static/js/5.a57c8684.chunk.js"
+  },
+  {
+    "revision": "9c8765ccd6d3bb5b8004",
+    "url": "/static/js/6.20d2c83b.chunk.js"
+  },
+  {
+    "revision": "3d4fb4189c8a322a3001",
+    "url": "/static/js/main.409af7e4.chunk.js"
+  },
+  {
+    "revision": "5728cf7960da1c909e7f",
+    "url": "/static/js/runtime-main.1dfba76f.js"
+  },
+  {
+    "revision": "5fd85b9e136074ed166f46c783115828",
+    "url": "/static/media/filipizen.5fd85b9e.svg"
+  },
+  {
+    "revision": "3a1a1c3516668b3b794547646d9d297e",
+    "url": "/static/media/logo~FfTuzhZc.3a1a1c35.png"
+  }
+]);
--- cloud-filipizen/client/service-worker.js
+++ cloud-filipizen/client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.06f464e5c38f6408d010d0991a234d4b.js"
+  "/precache-manifest.fd0275112e77bda43968f7640a124930.js"
 );
 
 self.addEventListener('message', (event) => {
