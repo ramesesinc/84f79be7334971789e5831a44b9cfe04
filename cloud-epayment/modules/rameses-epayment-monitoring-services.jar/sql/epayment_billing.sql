6611f7b61c59046a9b74c53f121a85d81d43e732
Jessie S. Zamora <jzamss@gmail.com>
2021-11-04 22:13:31 +0800

    cloud-epayment: billing: add merchant grouping

--- cloud-epayment/modules/rameses-epayment-monitoring-services.jar/sql/epayment_billing.sql
+++ cloud-epayment/modules/rameses-epayment-monitoring-services.jar/sql/epayment_billing.sql
@@ -18,10 +18,12 @@ select
 	po.phoneno,
 	po.origin,
 	case when po.webfee = 0 or po.webfee is null then 20 else po.webfee end as webfee,
-	po.checkouttype
+	po.checkouttype,
+	ppo.info
 from payment p 
 inner join paymentorder_paid po on p.paymentrefid = po.objid 
+inner join payment_partner_option ppo on p.orgcode = ppo.partnerid and p.paypartnerid = ppo.paypartnerid
 where p.paypartnerid = $P{paypartnerid}
 and p.tracedate >= $P{startdate} 
 and p.tracedate < $P{enddate}
-order by p.tracedate
+order by p.orgcode, p.tracedate
