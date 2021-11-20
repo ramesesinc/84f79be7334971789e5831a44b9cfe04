e797d205b807e29b0bcc2bbdf287daa9c42bd62b
Jessie S. Zamora <jzamss@gmail.com>
2021-11-20 15:09:04 +0800

    filipizen-client   * fix partner services when back button is click   * bpls: revert to 0.2.4

---
+++ cloud-filipizen/client/precache-manifest.81883244e1484d8a333854267e017510.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c371369edf24a51b2424c544ade72618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e301129a2ff78a89c7ca",
+    "url": "/static/css/2.756ae631.chunk.css"
+  },
+  {
+    "revision": "470fb0d3604a272c02c6",
+    "url": "/static/css/main.8d4fc355.chunk.css"
+  },
+  {
+    "revision": "e301129a2ff78a89c7ca",
+    "url": "/static/js/2.8780ccb4.chunk.js"
+  },
+  {
+    "revision": "c55fc84cbb1e709f2b38daf89ae8752e",
+    "url": "/static/js/2.8780ccb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ae6ac35e96563c2993",
+    "url": "/static/js/3.9a15ea98.chunk.js"
+  },
+  {
+    "revision": "9c5d521c0c848b15fdb1",
+    "url": "/static/js/4.9e91ba4a.chunk.js"
+  },
+  {
+    "revision": "8a71ce3b244b44561d1c",
+    "url": "/static/js/5.cfd07140.chunk.js"
+  },
+  {
+    "revision": "7833cbdf0849298a3872",
+    "url": "/static/js/6.a3b3f569.chunk.js"
+  },
+  {
+    "revision": "16545dd16a36c2d99e30",
+    "url": "/static/js/7.489be2cd.chunk.js"
+  },
+  {
+    "revision": "470fb0d3604a272c02c6",
+    "url": "/static/js/main.0d0589f8.chunk.js"
+  },
+  {
+    "revision": "ff08beeaa0fa931b68c6",
+    "url": "/static/js/runtime-main.58cd2244.js"
+  },
+  {
+    "revision": "5fd85b9e136074ed166f46c783115828",
+    "url": "/static/media/filipizen.5fd85b9e.svg"
+  },
+  {
+    "revision": "3a1a1c3516668b3b794547646d9d297e",
+    "url": "/static/media/logo~FfTuzhZc.3a1a1c35.png"
+  }
+]);
--- cloud-filipizen/client/service-worker.js
+++ cloud-filipizen/client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.42b0a846ef3488c28a3cf827e36a1b00.js"
+  "/precache-manifest.81883244e1484d8a333854267e017510.js"
 );
 
 self.addEventListener('message', (event) => {
