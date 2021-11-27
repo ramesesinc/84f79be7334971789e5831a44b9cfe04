fe613a628a37ae8c0277d9a493befe1be126b6d9
Jessie S. Zamora <jzamss@gmail.com>
2021-11-27 15:30:15 +0800

    filipizen-client: bpls renewal requirement update

--- cloud-filipizen/client/service-worker.js
+++ cloud-filipizen/client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c518e87b49d439e2bf5ff65e79b5979d.js"
+  "/precache-manifest.479f809cb1ee5aa775491504aba1ed79.js"
 );
 
 self.addEventListener('message', (event) => {
