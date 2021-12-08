c96c0f7e2a6d678b2dfd577ca9ff358a8ce9a441
worgflores <worgflores@gmail.com>
2021-12-09 00:44:58 +0800

    bin: updated

--- bin/restart-nginx.sh
+++ bin/restart-nginx.sh
@@ -4,4 +4,4 @@ cd ..
 BASE_DIR=`pwd`
 cd $BASE_DIR/nginx && docker-compose down
 cd $BASE_DIR/nginx && docker-compose -f docker-compose.yml -f docker-compose-private.yml up -d
-cd $BASE_DIR/nginx && docker-compose logs -f cloud-proxy-server
+cd $BASE_DIR/nginx && docker-compose logs -f
