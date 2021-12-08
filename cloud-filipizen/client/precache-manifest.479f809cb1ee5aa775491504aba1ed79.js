4b7495283354cbb5183a2cf77c28dfb62b82e5a0
worgflores <worgflores@gmail.com>
2021-12-08 23:33:25 +0800

    cloud-filipizen: updated

---
+++ cloud-filipizen/client/precache-manifest.479f809cb1ee5aa775491504aba1ed79.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20f17c04b8a11d90c54126eaaa752ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac9ce69da36f44a012a1",
+    "url": "/static/css/2.e319fea7.chunk.css"
+  },
+  {
+    "revision": "5d50e3bb0a8376172766",
+    "url": "/static/css/main.8d4fc355.chunk.css"
+  },
+  {
+    "revision": "ac9ce69da36f44a012a1",
+    "url": "/static/js/2.d9c3b8e2.chunk.js"
+  },
+  {
+    "revision": "c55fc84cbb1e709f2b38daf89ae8752e",
+    "url": "/static/js/2.d9c3b8e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ecd230e13a45e1742c4",
+    "url": "/static/js/3.da63527d.chunk.js"
+  },
+  {
+    "revision": "6bb665a7ecc118a9edb4",
+    "url": "/static/js/4.d70dfa9f.chunk.js"
+  },
+  {
+    "revision": "4fb089383f7a4a056a16",
+    "url": "/static/js/5.9d422e97.chunk.js"
+  },
+  {
+    "revision": "abf0700cd8784e3b3caf",
+    "url": "/static/js/6.b6437f6d.chunk.js"
+  },
+  {
+    "revision": "4254afcee8c2190dc3a0",
+    "url": "/static/js/7.287a030f.chunk.js"
+  },
+  {
+    "revision": "5d50e3bb0a8376172766",
+    "url": "/static/js/main.1f30b526.chunk.js"
+  },
+  {
+    "revision": "2a405f1ae403b3a9606f",
+    "url": "/static/js/runtime-main.009fcb28.js"
+  },
+  {
+    "revision": "5fd85b9e136074ed166f46c783115828",
+    "url": "/static/media/filipizen.5fd85b9e.svg"
+  },
+  {
+    "revision": "3a1a1c3516668b3b794547646d9d297e",
+    "url": "/static/media/logo~FfTuzhZc.3a1a1c35.png"
+  }
+]);
