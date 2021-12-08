139714cd1daf6251daeca826cc8325efc62e6fc0
worgflores <worgflores@gmail.com>
2021-12-09 00:45:56 +0800

    added

---
+++ cloud-bpls/modules/cloud-bpls-services.jar/sql/online_psic.sql
@@ -0,0 +1,22 @@
+[getSubclasses]
+select 
+	s.*
+from online_psic_subclass s 
+where s.name like $P{searchtext} 
+   or s.code like $P{searchtext}
+order by s.name
+
+
+[getClasses]
+select 
+	c.*,
+	g.objid as group_objid,
+	g.name as group_name,
+	d.objid as division_objid,
+	d.name as division_name
+from online_psic_class c 
+inner join online_psic_group g on c.parentid = g.objid 
+inner join online_psic_division d on g.parentid = d.objid
+where c.name like $P{searchtext}
+    or c.code like $P{searchtext}
+order by c.name
